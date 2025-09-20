8b2d0aa6e1c4404d6d3721586b5d20f60b303396
Thomas Ip <thomas@ipthomas.com>
2025-09-20 19:49:50 +0100

    Insert submitted email to D1

--- functions/signup.js
+++ functions/signup.js
@@ -9,5 +9,15 @@ export const onRequestPost = async (context) => {
   if (!validEmail(email)) {
     return new Response("Does not look like an email!", { status: 400 });
   }
-  return new Response(`Hello, ${email}`);
+  const { success } = await context.env.softlaunch_d1
+    .prepare("INSERT INTO signup (email) VALUES (?)")
+    .bind(email)
+    .run();
+  if (!success) {
+    return new Response("Something went wrong saving your email, please try again!", { status: 500 });
+  }
+  const redirectUrl = new URL(context.request.url);
+  redirectUrl.pathname = "/success";
+  redirectUrl.searchParams.set("email", email);
+  return Response.redirect(redirectUrl.toString(), 302);
 }
